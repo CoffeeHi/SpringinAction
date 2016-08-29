b63dc621d99da6cbf6773b18e7cdfa73ede11101
CoffeeHi <codercx@sina.com>
2016-08-29 17:55:36 +0800

    0829

--- src/main/java/com/five/RootConfig.java
+++ src/main/java/com/five/RootConfig.java
@@ -6,10 +6,14 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.tiles2.TilesConfigurer;
 import org.springframework.web.servlet.view.tiles2.TilesViewResolver;
+
+import java.io.IOException;
 
 /**
  * Created by 陈祥 on 2016/8/23.
@@ -38,5 +42,10 @@ public class RootConfig {
     @Bean
     public ViewResolver viewResolver(){
         return new TilesViewResolver();
+    }
+
+    @Bean //使用Servlet3.0解析multipart请求
+    public MultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
     }
 }
